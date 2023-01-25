6ca27d3b1c9f2570d706903b731c43bd210806d6
Aaryan <bhurtelaaryan00@gmail.com>
2023-01-25 18:00:31 +0545

    setting up OPD appoinment time:tomorrow

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random #adding libraries for later on
+import random  # adding libraries for later on
 print("Welcome to the VSS hospital please choose any one department")
 print("""
 1.)OPD
@@ -19,14 +19,32 @@ if user_input == "OPD":
     Later on 
     """)
     opd_time = input("")
+    # OPD doctor appoinment time
+    opd_time_clock= ["5:00 PM", "4 PM", "6PM", "7PM"]
+    # opd_today
     if opd_time == "Today" or "today":
         print("Checking for the appoinments of today")
         print("Here are the doctors of OPD departments avalable today")
-        doctors_opd_list= ["Dr Dina Sharma", "Dr Bisharath Thapaliya", "Dr Rajiv Pandey","Dr Sandip Raj Pandey","Dr Manish Koirala","Dr Sabal Bohora","Dr Bikash Bhattrai", "Uma Shakya"]
-        doctors_opd_print = random.sample(doctors_opd_list, 5)
-        print(doctors_opd_print)
+        doctors_opd_today_list = ["Dr Dina Sharma", "Dr Bisharath Thapaliya", "Dr Rajiv Pandey",
+                                  "Dr Sandip Raj Pandey", "Dr Manish Koirala", "Dr Sabal Bohora", "Dr Bikash Bhattrai", "Uma Shakya"]
+        doctors_opd_today_print = random.sample(doctors_opd_today_list, 5)
+        print(doctors_opd_today_print)
         print("Now since you have got the doctors please choose any one doctor for the appoinment")
-        doctor_opd_choose = input()
-        opd_time = ["5:00 PM", "4 PM","6PM","7PM"]
-        opd_time_print= random.choice(opd_time)
-        print(f"You have got an appoinment of {doctor_opd_choose} at {opd_time_print}")
+        doctor_opd_today_choose = input("")
+        opd_time_print = random.choice(opd_time_clock)
+        print(
+            f"You have got an appoinment of {doctor_opd_today_choose} at {opd_time_print} Department: OPD")
+
+    #OPD Tomorrow
+    if opd_time == "Tomorrow" or "tomorrow":
+        print("Checking for the appoinment for tomorrow")
+        print("Here are the doctors of OPD department who will be available tomorrow")
+        doctors_opd_tomorrow_list = [random.sample(doctors_opd_today_list,2), "Dr Rajesh Singh Thakuri", "Dr Ram Thapa" "Dr Laju Basnet", "Dr Rahul Sharma", "Dr Sumit Chaulagain"]
+        doctors_opd_tomorrow_print = random.sample(doctors_opd_tomorrow_list, 5)
+        print(doctors_opd_tomorrow_print)
+        print("Now since you have got the doctors please choose a doctor and we will let you know the appoinment time")
+        doctors_opd_tomorrow_choose = input("")
+        opd_time_print = random.choice = (opd_time_clock)
+        print(f"You have got an appoinment of {doctors_opd_tomorrow_choose} at {opd_time_print} Department : OPD")
+
+    
