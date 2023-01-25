ab9a85fb4ee2aec9094a4d9dacc19285fc291c92
Aaryan <bhurtelaaryan00@gmail.com>
2023-01-25 16:22:52 +0545

    adding departments and taking user input

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import random #adding libraries for later on
+print("Welcome to the VSS hospital please choose any one department")
+print("""
+1.) OPD
+2.) Pediatrics
+3.) Gynaecology
+4.)Radiology
+5.)ENT
+6.)Dental
+7.)Cardiology""")
+user_input = input("")
+print(f'>{user_input}')
